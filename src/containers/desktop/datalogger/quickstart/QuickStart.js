4f28b13ff43bc8ec624d596a4728b2eab0510808
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-11-01 09:19:15 +0700

    Quick start page

--- src/containers/desktop/datalogger/quickstart/QuickStart.js
+++ src/containers/desktop/datalogger/quickstart/QuickStart.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 function QuickStart(props) {
     return (
         <div className="main">
+            <div className="crumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <NavLink to="/datalogger">
+                            Daskboard
+                        </NavLink>
+                    </li>
+                    <li className="breadcrumb-item active">Quick Start</li>
+                </ol>
+            </div>
             <div className="quick-start">
-                Quick Start
             </div>
         </div>
     );
