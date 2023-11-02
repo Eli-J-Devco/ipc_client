de17c7a18766befc9b533214b67e6915167f0503
Hung Bui <hbui@nwemon.com>
2023-11-02 09:09:11 +0700

    WEBDEV-1086 - IPC - HTML Mockup RS485 page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import Scada from './layouts/scada/Scada';
 import ScadaDashboard from './containers/desktop/scada/daskboard/Dashboard';
 import Devices from './containers/desktop/datalogger/devices/Devices';
 import ScadaDevices from './containers/desktop/scada/devices/Devices';
+import RS485 from './containers/desktop/datalogger/RS485/RS485';
+import Device from './containers/desktop/datalogger/RS485/device/Device';
+import Options from './containers/desktop/datalogger/RS485/options/Options';
 
 export default class App extends Component {
   render() {
@@ -22,6 +25,11 @@ export default class App extends Component {
 
             <Route path='/datalogger' element = {<Datalogger />}>
               <Route index element = {<QuickStart />} />
+              <Route path='quickstart' element = {<QuickStart />} />
+              <Route path='rs485' element = {<RS485 />} >
+                <Route path=':id' element = {<Device />} />
+                <Route path='options' element = {<Options />} />
+              </Route>
               <Route path='devices' element = {<Devices />} />
             </Route>
 
