8492fd903cb3370b1a032b927512b5cdc727d656
Hung Bui <hbui@nwemon.com>
2023-11-02 09:02:13 +0700

    Add NavTabs component

---
+++ src/components/navTabs/NavTabs.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import styles from './NavTabs.module.scss';
+
+function NavTabs({ routes }) {
+    return (
+        <div className={styles.tabs}>
+            <ul className="nav nav-tabs border-bottom-0">
+                {
+                    routes.map(route => (
+                        <li className="nav-item">
+                            <NavLink to={route.path} className={(navData) => `nav-link m-0 border-0 ${navData.isActive ? styles.active : ""}`} end>
+                                {route.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                
+            </ul>
+        </div>
+    );
+}
+
+export default NavTabs;
