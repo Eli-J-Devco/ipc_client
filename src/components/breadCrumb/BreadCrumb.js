f1709b683acda01ca2f99bba4b7f7e4f3b6f72d4
Hung Bui <hbui@nwemon.com>
2023-11-04 13:07:16 +0700

    Update colors using variable

--- src/components/breadCrumb/BreadCrumb.js
+++ src/components/breadCrumb/BreadCrumb.js
@@ -7,7 +7,7 @@ function Breadcrumb({ routes }) {
             <ol className={`breadcrumb ${styles.breadcrumb}`}>
                 {
                     routes.map(route => (
-                        <li className="breadcrumb-item">
+                        <li key={route.path} className="breadcrumb-item">
                             <NavLink to={route.path} className={(navData) => navData.isActive ? styles.active : ""} end>
                                 {route.name}
                             </NavLink>
