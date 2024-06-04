2c9f0cd8da1fc23e22852b13611ca869fa2e5757
nartnahnnw <nhantran@nwemon.com>
2024-06-04 11:32:46 +0700

    Change api host

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -16,10 +16,9 @@ switch (url) {
     serverData = "";
     break;
   default:
-    // serverAPI = "http://115.78.133.129:3001";
+    serverAPI = "http://115.78.133.129:3001";
     // serverAPI = "http://127.0.0.1:3001";
-    serverAPI = "http://localhost:3001";
-    // serverAPI = "http://115.78.133.129:3001";
+    // serverAPI = "http://localhost:3001";
     serverData = "http://localhost:3015/uploads";
     break;
 }
